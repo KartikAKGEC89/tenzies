479c206187a0311b9e89288765e402a9e0f8285d
Kartik <s11kartik2111087@gmail.com>
2023-10-20 18:29:16 +0530

    Final

--- tenzies/src/App.js
+++ tenzies/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
 import Die from './Die';
-import {nanoid} from 'nanoid'
+import { nanoid } from 'nanoid'
+import Confetti from "react-confetti"
 
 function App() {
   const [dice, setDice] = React.useState(allNewDice())
+  const [tenzies, setTenzies] = React.useState(false)
+
+  React.useEffect( () => {
+    const allDice = dice.every(die => die.isHeld)
+    const firstValue = dice[0].value
+    const allValue = dice.every(die => die.value === firstValue)
+    
+    if (allDice && allValue) {
+      setTenzies(true)
+      console.log("you won")
+    }
+  }, [dice])
 
   function refresh() {
     // setDice(prevDice => {
@@ -15,13 +28,18 @@ function App() {
     // }
     // return arr
     // })
-    setDice(oldDice => oldDice.map(die => {
-      return die.isHeld ? die : {
-        value: Math.ceil(Math.random() * 6),
-        isHeld: false,
-        id: nanoid()
-      }
-    } ))
+    if (!tenzies) {
+      setDice(oldDice => oldDice.map(die => {
+        return die.isHeld ? die : {
+          value: Math.ceil(Math.random() * 6),
+          isHeld: false,
+          id: nanoid()
+        }
+      }))
+    } else {
+      setDice(allNewDice())
+      setTenzies(false)
+    }
   }
   
   function allNewDice() {
@@ -60,12 +78,13 @@ function App() {
   const Dice = dice.map(die => <Die key={die.id} value={die.value} isHeld={die.isHeld} hold={ () => hold(die.id)} />)
   return (
     <main>
+      {tenzies && <Confetti />}
       <h1 className="title">Tenzies</h1>
             <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
     <div className='die-container'>
       {Dice}
     </div>
-    <button className="roll-dice" onClick={refresh}>Roll</button>
+      <button className="roll-dice" onClick={refresh}>{ tenzies ? "New Game" : "Roll" }</button>
     </main>
      );
 }
