a8e762bc97da8bd4950868e10c0db94715632611
Kartik <s11kartik2111087@gmail.com>
2023-10-20 15:11:29 +0530

    id

--- tenzies/src/App.js
+++ tenzies/src/App.js
@@ -1,32 +1,38 @@
 import React from 'react';
 import './App.css';
 import Die from './Die';
+import {nanoid} from 'nanoid'
 
 function App() {
   const [dice, setDice] = React.useState(allNewDice())
 
   function refresh() {
-    setDice(prevDice => {
-      const arr = [] 
-    for (let i = 0; i < 10; i++)
-    {
-      arr.push(Math.ceil(Math.random()*6))
-    }
-    return arr
-    })
+    // setDice(prevDice => {
+    //   const arr = []
+    // for (let i = 0; i < 10; i++)
+    // {
+    //   arr.push(Math.ceil(Math.random()*6))
+    // }
+    // return arr
+    // })
+    setDice(allNewDice())
   }
   
   function allNewDice() {
     const arr = [] 
     for (let i = 0; i < 10; i++)
     {
-      arr.push(Math.ceil(Math.random()*6))
+      arr.push({
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid()
+      })
     }
     return arr
   }
   // console.log(allNewDice())
 
-  const Dice = dice.map(die => <Die value={die} />)
+  const Dice = dice.map(die => <Die key={die.id} value={die.value} />)
   return (
     <main className='die-container'>
       {Dice}
